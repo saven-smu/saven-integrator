dca16e489aae2e1e0ab556b602202aa317873ba0
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2022-10-26 00:24:32 +0800

    feat: add process leaderboard logic

--- src/sync_data.py
+++ src/sync_data.py
@@ -1,5 +1,5 @@
 from tasks.retrieve_bills import extract_users, process_bills, load_bills
-from tasks.compute_leaderboard import extract_bills
+from tasks.compute_leaderboard import extract_bills, process_leaderboard
 
 from prefect import flow
 
@@ -13,13 +13,13 @@ def retrieve_bills():
     load_bills(proc_df)
 
 @flow(name="Compute Leaderboard")
-def compute_leaderboard(type):
+def compute_leaderboard(time_window):
     # Extract bills from database
-    df = extract_bills(type)
+    bill_res = extract_bills(time_window)
     # Process bills for given user
-    # proc_df = process_bills(df)
+    lb_res = process_leaderboard(time_window, bill_res[1])
     # Load bills into database
-    # load_bills(proc_df)
+    ulb_df = process_userleaderboard(bill_res[0], lb_res[1])
 
 if __name__ == "__main__":
     # Test Flows
--- src/tasks/compute_leaderboard.py
+++ src/tasks/compute_leaderboard.py
@@ -3,7 +3,7 @@ from utils.statements import bills, date_range
 from prefect import task, get_run_logger
 from prefect.blocks.system import Secret
 from sqlalchemy import create_engine
-from pandas import read_sql
+from pandas import read_sql, DataFrame
 from datetime import datetime, timedelta
 import uuid
 
@@ -16,14 +16,47 @@ def extract_bills(type):
 
         date_tup = date_range(type)
         model = bills["get_bills"].format(date_tup[0], date_tup[1])
+        logger.info(model)
         df = read_sql(sql=model, con=conn)
 
         logger.info(f"Extracted User DF {df.shape}:" )
         logger.info(df.head())
-        return df
+        return (df, date_tup[0])
+
     except Exception as e:
         logger.error("Data extract error: " + str(e))
         raise ValueError()
     finally:
         if conn:
             conn.close()
+
+@task(name="Process Leaderboard")
+def process_leaderboard(tw, sdt):
+    try: 
+        logger = get_run_logger()
+
+        # Generate and rename id
+        df = DataFrame(data={'utility_type': [1, 2, 3, 4]})
+
+        # Generate id
+        ids = [uuid.uuid4() for _ in range(len(df.index))]
+        df["id"] = ids
+
+        # Insert stored_date_time and time_window
+        df["time_window"] = tw
+        df["stored_date_time"] = sdt
+
+        # Process date
+        current_datetime = datetime.now()
+        df["date_created"] = current_datetime
+        df["last_updated"] = current_datetime
+
+        df = df[['id', 'utility_type', 'time_window', 'stored_date_time', 'date_created', 'last_updated']]
+        logger.info(f"Process Leaderboard DF {df.shape}:")
+        logger.info(df.head())
+        return (df, ids)
+
+    except Exception as e:
+        logger.error("Data process error: " + str(e))
+        raise ValueError()
+
